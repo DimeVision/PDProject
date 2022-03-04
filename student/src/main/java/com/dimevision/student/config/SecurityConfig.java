66ef91f8f0674f92919f2b5231a1090088bf9a1d
dimevision <Fommkaa2304@mail.ru>
2022-03-04 13:44:10 +0300

    Added SpringSecurity configuration

---
+++ student/src/main/java/com/dimevision/student/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.dimevision.student.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Dimevision
+ * @version 0.1
+ */
+
+@Configuration
+public class SecurityConfig {
+    // TODO: 3/2/2022 Add and configurate spring security
+}
