a0535af45948bfc3695afb29d0c5b7491e7cd902
VLRCH <50802635+DimeVision@users.noreply.github.com>
2022-03-07 15:53:31 +0500

    Update StudentApplication.java

--- student/src/main/java/com/dimevision/student/StudentApplication.java
+++ student/src/main/java/com/dimevision/student/StudentApplication.java
@@ -1,1 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author Dimevision
+ * @version 0.1
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class StudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentApplication.class, args);
+    }
+}
